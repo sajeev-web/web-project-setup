037eae52ea0f3dac0012f6d845b5d4e50de5f7c1
sajeev-web <sajeev.www@gmail.com>
2018-06-08 21:29:09 +0400

    Added slider

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -70,7 +70,7 @@
             },
             CLASS_SELECTORS:
             {
-                
+                "singleItem": ".single-item"
             },
             URLS:
             {
@@ -82,23 +82,21 @@
             main: function()
             {
                 $(document).foundation();
-                if ($(CONSTANTS.ID_SELECTORS.name).length)
-                    this.functionName();
+                if ($(CONSTANTS.CLASS_SELECTORS.singleItem).length)
+                    this.initaliseSlider();
 
             },
-            functionName: function()
+            initaliseSlider: function()
             {
-                var that = this;
-                $(CONSTANTS.CUSTOM_SELECTORS.showResetPassword).on("click", function(e)
-                {
-                    e.preventDefault();
-
-                    that.clearResetPasswordMessages();
-                    $(CONSTANTS.ID_SELECTORS.resetWrapper).removeClass("hide");
-                    $(CONSTANTS.ID_SELECTORS.loginWrapper).addClass("hide");
-
-                    return false;
-                });
+                var $this = this;
+                $(CONSTANTS.CLASS_SELECTORS.singleItem).slick({
+                    dots: false,
+                    nextArrow: '<i class="fa fa-arrow-right"></i>',
+                    prevArrow: '<i class="fa fa-arrow-left"></i>',
+                    autoplay:true,
+                    autoplaySpeed:1500,
+                    arrows:true
+                }); 
             },
            
         }
